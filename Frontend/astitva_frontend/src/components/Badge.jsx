4d2e65b0032a1475058630f901ea9b70a35e9d2b
Varun Budhani <111705556+Vab1234@users.noreply.github.com>
2025-03-28 12:21:25 +0530

    Delete Frontend/astitva_frontend directory

--- Frontend/astitva_frontend/src/components/Badge.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function Badge({ className, children }) {
-    return (
-      <span className={`inline-flex items-center rounded-full px-4 py-2 text-sm font-medium ${className}`}>
-        {children}
-      </span>
-    );
-  }
--- Frontend/astitva_frontend/src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export function Card({ className, children }) {
-    return (
-      <div className={`bg-white rounded-xl shadow-sm ${className}`}>
-        {children}
-      </div>
-    );
-  }
